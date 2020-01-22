d253786403dcc183e87a9d3fe885adfd916acc10
Fraser Smith <smithfra@192-168-1-10.tpgi.com.au>
2020-01-22 20:48:07 +1100

    simpliied foreach loops

--- transit-gateway.ps1
+++ transit-gateway.ps1
@@ -5,9 +5,9 @@ $region = "ap-southeast-2"
 
 #fixed variables
 $transitgateway = "./files/transit-gateway.yaml"
-$source = "./files/source.yaml"
+$attachmentsource = "./files/attachment-source.yaml"
 $attachment = "./files/attachment.yaml"
-$Accounts = @( 
+$accounts = @( 
     [PSCustomObject]@{Account="SandboxICE"; VPC="vpc-07ef9b8277c459d64"; Subnet01="10.94.0.0/22"; Subnet02="10.94.4.0/22"},
     [PSCustomObject]@{Account="SandboxD3"; VPC="vpc-050885cb53b9a1d39"; Subnet01="10.95.0.0/22"; Subnet02="10.95.4.0/22"},
     [PSCustomObject]@{Account="PipelineProd"; VPC="vpc-e818128d"; Subnet01="10.64.100.0/23"; Subnet02="10.64.102.0/23"},
@@ -20,26 +20,31 @@ $Accounts = @(
 
 #deploy transit gateway in master account
 aws-role master master
-New-CFNStack -StackName $stackname -TemplateBody (Get-Content $transitgateway -raw) -Region ap-southeast-2
+New-CFNStack -StackName $stackname -TemplateBody (Get-Content $transitgateway -raw) -Region $region
 
-$transitgatewayID = (Get-EC2TransitGateway -Region ap-southeast-2 | ? Tags -like *$projectuuid* ).TransitGatewayId
-(Get-Content $source) -replace '$transitgatewayID',"$transitgatewayID" | Out-File $attachment -force
+$transitgatewayID = (Get-EC2TransitGateway -Region $region | ? Tags -like *$projectuuid* ).TransitGatewayId
 
-#connect to account to configure transit gateway attachment
+while($transitgatewayID -eq $null){sleep 1} #wait for transit gateway to provision
+
+#connect to each account and configure transit gateway attachment
 foreach($a in $accounts){
     
-    aws-role $a.Account admin #Connect to Account
+    #Connect to Account
+    aws-role $a.Account admin 
 
-    # Find and replace VPC, Subnets into final attachment.yaml file 
-    (Get-Content $attachment) -replace '$vpc',$a.VPC | Out-File $attachment -force
-    (Get-Content $attachment) -replace '$subnet01',$a.Subnet01 | Out-File $attachment -force
-    (Get-Content $attachment) -replace '$subnet02',$a.Subnet02 | Out-File $attachment -force
-    (Get-Content $attachment) -replace '$uuid',$uuid | Out-File $attachment -force
+    # Find and replace vpc, subnet01, subnet02 and project uuid and output to attachment.yaml
+    (Get-Content $attachmentsource) | Foreach-Object {
+        $_ -replace '$transitgatewayID',"$transitgatewayID" `
+           -replace '$vpc',$a.VPC `
+           -replace '$subnet01',$a.Subnet01 `
+           -replace '$subnet02',$a.Subnet02 `
+           -replace '$uuid',$uuid
+        } | Set-Content $attachment -force
 
     New-CFNStack -StackName "$stackname-attachment" -TemplateBody (Get-Content $attachment -raw) -Region $region
-
+    Remove-Item $attachment -force #cleanup
             }
 
-#Approve-EC2TransitGatewayPeeringAttachment -TransitGatewayAttachmentId $transitgatewayID -Region ap-southeast-2
+Approve-EC2TransitGatewayPeeringAttachment -TransitGatewayAttachmentId $transitgatewayID -Region $region
 
 
